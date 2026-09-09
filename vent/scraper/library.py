c2472c1eee310e75e3b27fb1e802c67a82e404d6
Habibur Rahman <habib@isoldien.com>
2026-09-09 22:25:01 +0100

    finish library.py, get profile games which needs a API Key anyway so

--- vent/scraper/library.py
+++ vent/scraper/library.py
@@ -11,8 +11,16 @@ from vent.scraper.models import OwnedGame
 
 def from_profile(client: SteamClient, steam_id: int) -> list[OwnedGame]:
     """Return games owned by a steam profile using API Key."""
-    client.get_json(
+    owned_games: list[OwnedGame] = []
+    response = client.get_json(
         "IPlayerService/GetOwnedGames/v1",
         params={"steamid": steam_id, "include_appinfo": 1},
     )
-    raise NotImplementedError
+    
+    games_list = response.get("response", {}).get("games", [])
+    
+    for game in games_list:
+        game_object = OwnedGame(**game)
+        owned_games.append(game_object)
+    # @TODO: change this to list comprehension one day
+    return owned_games
